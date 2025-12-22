1e60ca9ee5ae2cc41880c46ee0ae7daa52ed7ac2
Martin <ickobombata@gmail.com>
2025-12-22 12:42:36 +0200

    use hash routing

--- frontend/car-service-frontend/src/app/app-routing.module.ts
+++ frontend/car-service-frontend/src/app/app-routing.module.ts
@@ -23,6 +23,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
+    useHash: true,
     anchorScrolling: 'enabled',
     scrollPositionRestoration: 'enabled'
   })],
